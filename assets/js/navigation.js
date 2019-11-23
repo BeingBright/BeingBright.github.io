6a00ec68d3379ae27330301b422c021e5781d0d4
BeingBright <BrightonvandenEnd@gmail.com>
2019-11-23 17:24:08 +0100

    current page highlights

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,12 +1,27 @@
 $(document).ready(function () {
-    $('h1').each(function (i, obj) {
+    $('.content').each(function (i, obj) {
         let base = $(this);
-        let link = $("<a page-link></a>").text(base.text())
+        let link = $("<a page-link></a>").text(base.attr("name"))
+        link.attr("id", base.attr("id"))
         link.click(function () {
             $([document.documentElement, document.body]).animate({
-                scrollTop: base.offset().top -  $('nav').height()
+                scrollTop: base.offset().top - $('nav').height()
             }, 1000)
         })
         $('nav').append(link)
     });
+});
+
+$(window).scroll(function () {
+    var current = 9999999999;
+    var obj;
+    $('.content').each(function () {
+        var c = -($(this).offset().top - $(window).scrollTop())
+        $('#' + $(this).attr("id")).removeAttr("select", "")
+        if (c < current && c >= 0) {
+            current = c
+            obj = $('#' + $(this).attr("id"))
+        }
+    });
+    obj.attr("select", "")
 });
